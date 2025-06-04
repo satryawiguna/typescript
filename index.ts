3e1b54dce8d2e7b881af3ca623212d1d9e764741
Satrya Wiguna <satrya@innoxcell.net>
2025-06-04 08:56:32 +0800

    Catch the error on typescript

--- index.ts
+++ index.ts
@@ -3,3 +3,8 @@ const students = [
   { name: "Bob", age: 22, grade: "B" },
   { name: "Charlie", age: 23, grade: "C" },
 ];
+
+for (const student of students) {
+  const { name, age, grade } = student;
+  console.log(`Name: ${name}, Age: ${age}, Grade: ${type}`);
+}
